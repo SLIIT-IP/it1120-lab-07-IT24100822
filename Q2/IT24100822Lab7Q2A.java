f8b44fe275ddce4e5926e1a0f54cff094377f211
IT24100822 <IT24100822@my.sliit.lk>
2024-09-29 22:42:32 +0530

    Add IT24100822Lab7Q2A.java to Q2

---
+++ Q2/IT24100822Lab7Q2A.java
@@ -0,0 +1,18 @@
+public class IT24100822Lab7Q2A
+{
+    public static void main(String[] args)
+    {
+        // Loop for 4 rows
+        for (int i = 1; i <= 4; i++) 
+        {
+            // Loop for 5 columns
+            for (int j = 1; j <= 5; j++)
+            {
+              System.out.print("$");
+                
+            }
+            
+            System.out.println();
+        }
+    }
+}
