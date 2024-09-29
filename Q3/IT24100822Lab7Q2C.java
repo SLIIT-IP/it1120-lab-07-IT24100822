c778191131238e45379b81ce7fc657c127199785
IT24100822 <IT24100822@my.sliit.lk>
2024-09-29 23:08:47 +0530

    Add IT24100822Lab7Q2C.java to Q3

---
+++ Q3/IT24100822Lab7Q2C.java
@@ -0,0 +1,12 @@
+public class IT24100822Lab7Q2C {
+    public static void main(String[] args) {
+        // Loop for rows
+        for (int i = 5; i >= 1; i--) {
+            // Loop for printing the numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
